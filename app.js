5fe8c599b05b51671700c2e83b84ac041817c64f
tomo watanabe <t.watanabe@freebit.net>
2014-02-25 13:15:11 +0900

    Apply simpleset

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server); // Socket.io
 
+var sets = require('simplesets');
+
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -37,18 +39,53 @@ app.get('/', routes.index);
 server.listen(app.get('port'), '::');
 console.log('Listening...');
 
+// iBeaconのスキャンを開始
+Bleacon.startScanning();
+
+var devSet = new sets.Set();
+var devMap = new Object();
+var currentDevice = new Array();
+
 // iBeaconの検索
 Bleacon.on('discover', function(bleacon) {
-    console.log('discover');
-    io.sockets.json.emit('message', { value: bleacon });
+
+    // 見つかったデバイスのkeyを作成
+    var key = bleacon.name + bleacon.uuid + bleacon.major;
+
+    // 同じデバイスが無ければ追加
+    if (!devSet.has(key)) {
+        devSet.add(key);
+        devMap[key] = bleacon.proximity;
+        currentDevice.push(bleacon);
+    }
+    // 同じデバイスがあった場合
+    else {
+        // Unknownになった場合は削除してしまおう
+        if (bleacon.proximity === 'unknown') {
+            console.log(key + ': ' + devMap[key]  + ' to '+ bleacon.proximity);
+            devSet.remove(key);
+            devMap[key].remove();
+        }
+        // デバイスのProximityが変更された
+        if (devMap[key] !== bleacon.proximity) {
+            console.log(key + ': ' + devMap[key]  + ' to '+ bleacon.proximity);
+            devMap[key] = bleacon.proximity;
+        }
+    }
+
+    // 現在の検索済みのデバイスを表示
+//    console.log(currentDevice.length);
+//    console.dir(currentDevice);
 });
 
 // クライアントが接続してきたときの処理
 io.sockets.on('connection', function(socket) {
     console.log("connect");
 
-    // iBeaconのスキャンを開始
-    Bleacon.startScanning();
+    // 見つかったデバイスを接続してきたブラウザに送信
+    for (var i in  currentDevice) {
+        socket.json.emit('message', { value: currentDevice[i] });
+    }
 
     // メッセージを受けた時の処理
     socket.on('message', function(data) {
